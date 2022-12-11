6c31d5edb275e29768a0615fe8c3bea7567fc2e7
Aimeri Baddouh <aimeri@baddouh.me>
2022-12-11 13:57:44 +0000

    feat: use cross platform open command

--- src/commands/run.rs
+++ src/commands/run.rs
@@ -8,11 +8,12 @@ pub(crate) fn run_project() -> Result<(), Report> {
     guard_toybox_pdxinfo_present()?;
     build_project()?;
     let project_name = parse_game_name_from_toybox_pdxinfo()?;
-    let emulator_output = std::process::Command::new("open")
-        .arg(format!("target/{project_name}.pdx"))
-        .output()
-        .expect("failed to execute process");
-    if !emulator_output.status.success() {
+    let emulator_output = open::that(format!("target/{project_name}.pdx"));
+    // let emulator_output = std::process::Command::new("open")
+    //     .arg(format!("target/{project_name}.pdx"))
+    //     .output()
+    //     .expect("failed to execute process");
+    if !emulator_output.is_err() {
         return Err(eyre!("emulator exited with error"))
             .with_section(move || {
                 "The emulator is the program used to run Toybox projects."
