17dbdf9bdd024b9575dff000d20d2b2919dd87d0
Aimeri Baddouh <aimeri@baddouh.me>
2022-12-11 14:58:43 +0000

    chore: clean up game launching logic

--- src/commands/run.rs
+++ src/commands/run.rs
@@ -1,5 +1,5 @@
 use crate::utils::{guard_toybox_pdxinfo_present, parse_game_name_from_toybox_pdxinfo};
-use color_eyre::{eyre::eyre, eyre::Report, Section, SectionExt};
+use color_eyre::{eyre::Report, Section, SectionExt};
 use owo_colors::OwoColorize;
 
 use super::build::build_project;
@@ -8,24 +8,35 @@ pub(crate) fn run_project() -> Result<(), Report> {
     guard_toybox_pdxinfo_present()?;
     build_project()?;
     let project_name = parse_game_name_from_toybox_pdxinfo()?;
-    let emulator_output = open::that(format!("target/{project_name}.pdx"));
+    open::that(format!("target/{project_name}.pdx"))
+        .with_section(move || {
+            "The emulator is the program used to run Toybox projects."
+                .header("Explanation:".yellow())
+        })
+        .with_section(move || {
+            format!(
+                "Try installing the emulator using \"{}\".",
+                "cargo install emulator".green()
+            )
+            .header("Suggestions:".green())
+        })?;
     // let emulator_output = std::process::Command::new("open")
     //     .arg(format!("target/{project_name}.pdx"))
     //     .output()
     //     .expect("failed to execute process");
-    if !emulator_output.is_err() {
-        return Err(eyre!("emulator exited with error"))
-            .with_section(move || {
-                "The emulator is the program used to run Toybox projects."
-                    .header("Explanation:".yellow())
-            })
-            .with_section(move || {
-                format!(
-                    "Try installing the emulator using \"{}\".",
-                    "cargo install emulator".green()
-                )
-                .header("Suggestions:".green())
-            });
-    }
+    // if !emulator_output.is_err() {
+    //     return Err(eyre!("emulator exited with error"))
+    //         .with_section(move || {
+    //             "The emulator is the program used to run Toybox projects."
+    //                 .header("Explanation:".yellow())
+    //         })
+    //         .with_section(move || {
+    //             format!(
+    //                 "Try installing the emulator using \"{}\".",
+    //                 "cargo install emulator".green()
+    //             )
+    //             .header("Suggestions:".green())
+    //         });
+    // }
     Ok(())
 }
